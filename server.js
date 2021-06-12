bdd6bf28842a24752bd28ea9978c1a108bac188d
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-06-12 11:37:21 -0500

    added login and logout

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 const path = require('path');
 const hbs = exphbs.create({});
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -15,6 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session(sess));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
