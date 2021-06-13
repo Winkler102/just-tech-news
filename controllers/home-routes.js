878b80e84c9e17d1eb8485810e600c2c262ae877
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-06-13 13:39:48 -0500

    added commenting and made login logout change based on user

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,6 @@ const { Post, User, Comment } = require('../models');
 
 
 router.get('/', (req, res) => {
-    console.log(req.session);
     Post.findAll({
         attributes: [
             'id',
@@ -30,7 +29,10 @@ router.get('/', (req, res) => {
     })
         .then(dbPostData => {
             const posts = dbPostData.map(post => post.get({ plain: true }));
-            res.render('homepage', { posts });
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
         })
         .catch(err => {
             console.log(err);
@@ -82,7 +84,10 @@ router.get('/post/:id', (req, res) => {
 
             const post = dbPostData.get({ plain: true });
 
-            res.render('single-post', { post });
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
         })
         .catch(err => {
             console.log(err);
