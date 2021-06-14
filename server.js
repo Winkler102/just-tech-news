d29072a652fa989bfc00c5cfb85802b5ae8ae2b6
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-06-14 12:21:28 -0500

    Added formating to comments and posts

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const path = require('path');
-const hbs = exphbs.create({});
 const routes = require('./controllers');
+const helpers = require('./utils/helper');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
