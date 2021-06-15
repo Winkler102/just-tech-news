18908a23abf2120488e20cdaf7fab312c7347538
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-06-15 11:47:31 -0500

    added authintication

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const dashboardRoutes = require('./dashboard-routes.js');
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
@@ -6,6 +7,7 @@ const homeRoutes = require('./home-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
